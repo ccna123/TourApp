1ec09d65c125885eeeaeee741f2184870393d67c
THANH <95860402+ccna123@users.noreply.github.com>
2024-03-24 20:05:29 +0900

    update

--- obj/Debug/net8.0/TourApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TourApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e6532b80aacf77f3ad662c4198aeeba09af583")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
